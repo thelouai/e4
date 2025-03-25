0575a224b57732339ea40ffd7efe614012ad4b2a
thelouai <122991749+thelouai@users.noreply.github.com>
2025-03-25 10:34:18 +0100

    Delete quantum.js

--- quantum.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dateElement = document.getElementById("current-date");
-    const options = { year: "numeric", month: "long" }; // Ex : Mars 2025
-    const currentDate = new Date().toLocaleDateString("fr-FR", options);
-    dateElement.textContent = currentDate;
-});
